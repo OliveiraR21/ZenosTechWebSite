78be96e3e837a382136598daf3b820cd9e693054
Renan José de Oliveira <oliveiira2012@gmail.com>
2026-02-19 14:27:41 +0000

    // For Firebase JS SDK v7.20.0 and later, measurementId is optional cons

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-8522279776-b07d8",
-  "appId": "1:955636879726:web:c19695ca00e3df76c0717f",
-  "apiKey": "AIzaSyAffqfNA7OrzKrxPnVETGfhpQYw_d2_sv0",
-  "authDomain": "studio-8522279776-b07d8.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "955636879726"
+  apiKey: "AIzaSyDwHhRkwHZ3vc2RYjRKPj40RScUltXMpA4",
+  authDomain: "zenos-tech-site.firebaseapp.com",
+  projectId: "zenos-tech-site",
+  storageBucket: "zenos-tech-site.firebasestorage.app",
+  messagingSenderId: "40631524019",
+  appId: "1:40631524019:web:5ef9187172e64fb67a2ec6",
+  measurementId: "G-J27W1M71J5"
 };
